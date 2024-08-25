e63b5603af57278183c932be0ace7d55b3a7ee8c
Fernando Romo <112670507+fromo0214@users.noreply.github.com>
2024-08-25 15:41:54 -0700

    Add files via upload

---
+++ personal-portfolio/myJSfile.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Optional: Close the modal when clicking outside of the modal content
+window.onclick = function(event) {
+    let modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
